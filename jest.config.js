87820f2a6dd40e3a934d99e6b52a36caf56d8621
André Campestrini <andre.bnu3@gmail.com>
2020-08-22 18:16:03 -0500

    feat: add setupTests file to jest

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  moduleNameMapper: {
+    "@/(.*)": "<rootDir>/src/$1",
+  },
   preset: "ts-jest",
+  setupFilesAfterEnv: ["./src/setupTests.ts"],
   testEnvironment: "node",
 };
