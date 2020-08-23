65dbec004140b45ce6677fb4a02c318e15eb449c
André Campestrini <andre.bnu3@gmail.com>
2020-08-22 22:39:44 -0500

    feat: improve error handling on request function

--- src/infrastructure/transport/http/FetchHttpClient.ts
+++ src/infrastructure/transport/http/FetchHttpClient.ts
@@ -1,8 +1,13 @@
-import { HttpClient, RequestParams } from "@/data/transport/http";
+import { HttpClient, HttpError, RequestParams } from "@/data/transport/http";
 
 export class FetchHttpClient implements HttpClient {
   async request<R>({ url, body, headers, method }: RequestParams): Promise<R> {
-    const request = await fetch(url, { body, headers, method });
-    return request.json();
+    const response = await fetch(url, { body, headers, method })
+
+    if (response.ok) {
+      return response.json();
+    }
+
+    throw new HttpError(response.status);
   }
 }
