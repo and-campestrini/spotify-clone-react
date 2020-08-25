0bde939b93668872ca5ef30878f5a363c78d676c
André Campestrini <andre.bnu3@gmail.com>
2020-08-24 23:33:19 -0500

    feat: add spotify oauth url to application config

--- src/data/config/AppConfig.ts
+++ src/data/config/AppConfig.ts
@@ -1,3 +1,4 @@
 export interface AppConfig {
   getSpotifyClientId(): string;
+  getSpotifyOAuthUrl(): string;
 }
--- src/infrastructure/config/DefaultAppConfig.ts
+++ src/infrastructure/config/DefaultAppConfig.ts
@@ -1,7 +1,13 @@
 import { AppConfig } from "@/data/config";
+import { injectable } from "tsyringe";
 
+@injectable()
 export class DefaultAppConfig implements AppConfig {
   getSpotifyClientId(): string {
     return "e2107d7460c146748662464f2cd498dc";
+  }
+
+  getSpotifyOAuthUrl(): string {
+    return "https://accounts.spotify.com/authorize";
   }
 }
